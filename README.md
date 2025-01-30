f7e988e595474135d8c438b7d66c943f081567ee
Guilherme <202311190034@ifba.edu.br>
2025-01-30 13:42:58 -0300

    Projeto T5U4_2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # EmbarcaTechT5U4_2
 Projeto realizado de acordo as instruções da atividade referente ao Capítulo 5 da Unidade 4. Esta referência se aplica ao programa de capacitação EmbarcaTech - TIC 37
+
+# Instruções
+O programa pode ser utilizado tanto em ambiente de simulador, utilizando a extensão Wokwi Simulator do VSCode, como na placa de desenvolvimento BitDogLab. <br>
+O projeto visa aplicar o conceito de alarme apresentado durante o curso, utilizando uma interrupção gerada após 3 segundos que o botão foi pressionado.<br>
+Caso utilize a placa BitDogLab, o botão utilizado foi o Botao_A. <br>
+Após o pressionamento do botão, é imprimida uma mensagem via comunicação serial informando que todos os LEDs foram ligados. Além da mudança de estado dos LEDs, é setado um alarme para 3 segundos. Este, por fim, irá iniciar a sequência de desligamento dos LEDs um por um, verificando qual LED está sendo desligado e imprimindo uma mensagem o indicando. Dentro desta função é definido outro alarme para desligar o LED seguinte, o processo se repete até que todos estejam desligados.
---
+++ T5U4_2.c
@@ -0,0 +1,77 @@
+// Inclusão de bibliotecas
+#include <stdio.h>
+#include "pico/stdlib.h"
+// Define os pinos do botão e dos LEDs
+#define BUTTON_PIN 5
+#define LED_PIN_GREEN 11
+#define LED_PIN_BLUE 12
+#define LED_PIN_RED 13
+// Variável de controle do contador
+static uint cont = 0;
+
+// Função de callback do alarme
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    cont++; // Incrementa o contador a cada execução
+    if (cont == 1) { // 1ª execução - Desliga LED verde
+        printf("Desligando LED verde\n");
+        gpio_put(LED_PIN_GREEN, 0);
+        gpio_put(LED_PIN_BLUE, 1);
+        gpio_put(LED_PIN_RED, 1);
+        add_alarm_in_ms(3000, alarm_callback, NULL, true);
+    } else if (cont == 2) { // 2ª execução - Desliga LED azul
+        printf("Desligando LED azul\n");
+        gpio_put(LED_PIN_GREEN, 0);
+        gpio_put(LED_PIN_BLUE, 0);
+        gpio_put(LED_PIN_RED, 1);
+        add_alarm_in_ms(3000, alarm_callback, NULL, true);
+    } else if (cont == 3) { // 3ª execução - Desliga LED vermelho
+        printf("Desligando LED vermelho\n");
+        gpio_put(LED_PIN_GREEN, 0);
+        gpio_put(LED_PIN_BLUE, 0);
+        gpio_put(LED_PIN_RED, 0);
+        cont = 0; // Reseta o contador
+    }
+return 0;
+}
+
+// Função de setup
+void setup() {
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
+    gpio_init(LED_PIN_GREEN);
+    gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
+    gpio_put(LED_PIN_GREEN, 0);
+
+    gpio_init(LED_PIN_BLUE);
+    gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
+    gpio_put(LED_PIN_BLUE, 0);
+
+    gpio_init(LED_PIN_RED);
+    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_put(LED_PIN_RED, 0);
+}
+
+// Função principal
+int main()
+{
+    stdio_init_all(); // Inicializa a comunicação serial
+    setup(); // Chama a função de setup
+
+    // Loop principal
+    while (true) {
+        if (!gpio_get(BUTTON_PIN) && !gpio_get(LED_PIN_RED)) { // Leitura do botão e do último LED aceso
+            sleep_ms(100); // debounce
+            if(!gpio_get(BUTTON_PIN)){ //Releitura do botão
+                printf("Ligando LEDs\n"); // Mensagem de depuração
+                // Liga os LEDs
+                gpio_put(LED_PIN_GREEN, 1);
+                gpio_put(LED_PIN_BLUE, 1);
+                gpio_put(LED_PIN_RED, 1);
+                add_alarm_in_ms(3000, alarm_callback, NULL, true); // Inicia a sequência de desligamento através de alarme
+            }
+        }
+    sleep_ms(10); // Evitar leituras em excesso
+    }
+}
